3b519b0392a6d63aced887cc824115c6940d8ea0
Peacearth <staratzhwiki@gmail.com>
2022-03-12 15:47:13 -0500

    Update articlestowrite.py

--- articlestowrite.py
+++ articlestowrite.py
@@ -5,16 +5,20 @@ siteen = pywikibot.Site('en','wikipedia')
 sitezh = pywikibot.Site('zh','wikipedia')
 
 projname = 'physics'
-projnamezh = '物理專題'
+projname_qual = projname + ' vital'
+projname_impo = projname
+projnamezh = '[[WikiProject:物理学|物理專題]]'
+en_maxsize = 20000 #20000
+zh_maxsize = 3000 #3000
 
-cat_fa = pywikibot.Category(siteen,'Category:FA-Class '+ projname +' articles')
-cat_fl = pywikibot.Category(siteen,'Category:FL-Class '+ projname +' articles')
-cat_ga = pywikibot.Category(siteen,'Category:GA-Class '+ projname +' articles')
-cat_a = pywikibot.Category(siteen,'Category:A-Class '+ projname +' articles')
-cat_b = pywikibot.Category(siteen,'Category:B-Class '+ projname +' articles')
-cat_top = pywikibot.Category(siteen,'Category:Top-importance '+ projname +' articles')
-cat_high = pywikibot.Category(siteen,'Category:High-importance '+ projname +' articles')
-cat_mid = pywikibot.Category(siteen,'Category:Mid-importance '+ projname +' articles')
+cat_fa = pywikibot.Category(siteen,'Category:FA-Class '+ projname_qual +' articles')
+cat_fl = pywikibot.Category(siteen,'Category:FL-Class '+ projname_qual +' articles')
+cat_ga = pywikibot.Category(siteen,'Category:GA-Class '+ projname_qual +' articles')
+cat_a = pywikibot.Category(siteen,'Category:A-Class '+ projname_qual +' articles')
+cat_b = pywikibot.Category(siteen,'Category:B-Class '+ projname_qual +' articles')
+cat_top = pywikibot.Category(siteen,'Category:Top-importance '+ projname_impo +' articles')
+cat_high = pywikibot.Category(siteen,'Category:High-importance '+ projname_impo +' articles')
+cat_mid = pywikibot.Category(siteen,'Category:Mid-importance '+ projname_impo +' articles')
 
 page_to_write = pywikibot.Page(sitezh, u"User:和平奮鬥救地球/sandbox_2")
 
@@ -27,7 +31,8 @@ gen_top = pagegenerators.CategorizedPageGenerator(cat_top, recurse=True)
 gen_high = pagegenerators.CategorizedPageGenerator(cat_high, recurse=True)
 gen_mid = pagegenerators.CategorizedPageGenerator(cat_mid, recurse=True)
 
-writestr = projnamezh + '待撰條目（英文B級且中重要度以上、少於20,000位元組；中文小於3,000位元組）：\n\n'
+#writestr = projnamezh + '待撰條目（英文B級且中重要度以上、少於20,000位元組；中文小於3,000位元組）：\n\n'
+writestr = projnamezh + '待撰條目（英文B級且中重要度以上、少於 '+ "{:,}".format(en_maxsize) +' 位元組；中文小於 '+ "{:,}".format(zh_maxsize) +' 位元組）：\n\n'
 writestr += '最後更新時間：~~~~~\n\n'
 
 writestr += '== 已有中文對應條目 ==\n'
@@ -95,7 +100,7 @@ for listimp in [listtop,listhigh,listmid]:
 				page_zh = pywikibot.Page(sitezh,ll.title)
 				zhleng=len(page_zh.text.encode("utf8"))
 				break
-		if zhleng>3000 or leng>20000: continue
+		if zhleng>zh_maxsize or leng>en_maxsize: continue
 		print(format(percentage, '0.3f'),'%:',enname,zhname,qual,impo,leng)
 		if zhleng>1: writestr += '|-\n|[[:en:' + enname + ']]||' + zhname + '||' + qual + '||' + impo + '||' + str(leng)  + '||' + str(zhleng)  + '\n'
 		else: writestr1 += '|-\n|[[:en:' + enname + ']]||' + qual + '||' + impo + '||' + str(leng) + '\n'
@@ -108,5 +113,5 @@ writestr+='\n'
 writestr+=writestr1
 
 page_to_write.text = writestr
-page_to_write.save(u"使用[[mw:Manual:Pywikibot/zh|Pywikibot]]更新數據")
+page_to_write.save(u"使用[[mw:Manual:Pywikibot/zh|Pywikibot]]更新數據：" + projnamezh)
 print('Done')
